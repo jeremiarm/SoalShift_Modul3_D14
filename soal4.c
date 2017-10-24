8cce7077029ae9e001f2572eb3503655ee2748d9
jeremiarm <jeremiarm5@gmail.com>
2017-10-24 23:45:08 +0700

    fitur scanf untuk nomor 4

--- soal4.c
+++ soal4.c
@@ -3,3 +3,28 @@
 #include<pthread.h>
 #include<stdlib.h>
 #include<unistd.h>
+
+pthread_t thd;
+int banyak=0;
+char check;
+int angka[1000];
+int res[1000];
+int input;
+int maxi=-1;
+void* fakt(void *arg)
+{
+   
+    return NULL;
+}
+
+int main(){
+	int i;
+	for(i=0;i<1000;i++)angka[i]=0;
+	while(1){
+	   scanf("%d%c",&input,&check);
+	   if(maxi<input)maxi=input;
+	   banyak++;
+	   angka[input]=1;
+	  if(check=='\n')break;
+	}
+}
