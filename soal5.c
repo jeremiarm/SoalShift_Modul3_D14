6b094efdc215538640b0d8ff1088769004dbe1b8
jeremiarm <jeremiarm5@gmail.com>
2017-10-25 13:47:49 +0700

    soal5.c baru

---
+++ soal5.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<string.h>
+#include<pthread.h>
+#include<stdlib.h>
+#include<unistd.h>
+
+
+pthread_t tid;
+int banyak[100];
+
+void* check(void *arg)
+{
+    while((read=getline(&line,&len,in)!=1)
+    {
+    }
+    return NULL;
+}
