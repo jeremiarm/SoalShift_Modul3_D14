c38ba8e16e92c2e8b383d69f2eb92bbd0d6fc7fb
jeremiarm <jeremiarm5@gmail.com>
2017-10-25 10:39:40 +0700

    soal4.c lengkap sudah jadi

--- soal4.c
+++ soal4.c
@@ -4,33 +4,38 @@
 #include<stdlib.h>
 #include<unistd.h>
 
-pthread_t thd;
-int banyak=0;
-char check;
+pthread_t tid;
 int angka[1000];
-int res;
-int input;
+long double res;
 int maxi=-1;
 int now;
 void* fakt(void *arg)
 {
    int q;
-   res=1;
+   res=(long double)1;
    for(q=now;q>=1;q--){
-       res*=q;
+       res=res*(long double)q;
    }
-   printf("Hasil %d! = %d\n",now,res);
+   printf("Hasil %d! = %0.Lf\n",now,res);
     return NULL;
 }
 
-int main(){
+int main(int argc, char** argv){
 	int i;
-	for(i=0;i<1000;i++)angka[i]=0;
-	while(1){
-	   scanf("%d%c",&input,&check);
-	   if(maxi<input)maxi=input;
-	   banyak++;
-	   angka[input]++;
-	  if(check=='\n')break;
+	int ubah;
+	for(i=0;i<1000;i++){
+		angka[i]=0;
+	}
+	for(i=1;i<argc;i++){
+		ubah=atoi(argv[i]);
+		angka[ubah]++;
+		if(maxi<ubah)maxi=ubah;
+	}
+	for(i=1;i<=ubah;i++){
+		now=i;
+		while(angka[now]--){
+			pthread_create(&(tid),NULL,&fakt,NULL);
+			pthread_join(tid,NULL);
+		}
 	}
 }
