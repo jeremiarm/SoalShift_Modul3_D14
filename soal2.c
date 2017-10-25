c3fc8c30431358b7e2ed20c8506fab26efae6f61
jeremiarm <jeremiarm5@gmail.com>
2017-10-25 11:58:07 +0700

    soal2.c fungsi turn player1

--- soal2.c
+++ soal2.c
@@ -3,3 +3,58 @@
 #include<pthread.h>
 #include<stdlib.h>
 #include<unistd.h>
+
+pthread_t tid1;
+pthread_t tid2;
+int status;
+int player=0;
+int input;
+int lubang1[10];
+int lubang2[10];
+int angka;
+int check;
+int breaker=0;
+char player1[100];
+char player2[100];
+int point1=0;
+int point2=0;
+void* turn1(void *arg)
+{
+    status = 0;
+    while(status==1){}
+    printf("Masukkan jumlah lubang yang diisi(1-4) %s\n",player1);
+    scanf("%d",&input);
+    check=1;
+    while(input){
+        printf("Cara ke-%d: ",check);
+        scanf("%d",angka);
+        angka--;
+        if(lubang1[angka]==1){
+                printf("Lubang udah ada\n");
+        }
+        else
+        {
+                lubang1[angka]=1;
+                breaker++;
+                check++;
+        }
+    }
+    printf("Masukkan angka oleh %s:  ",player2);
+    while(4--){
+   	 scanf("%d",&angka);
+   	 if(lubang1[angka]==1)point1++;
+         else point2++;
+    }
+    status = 1;
+ 
+    return NULL;
+}
+
+void* turn2(void *arg)
+{
+    status = 0;
+ 
+    status = 1;
+ 
+    return NULL;
+}
