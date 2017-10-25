f04b0f77cf61fa8d1d1e52eed2d0bd2dad94bb0a
muhilmi <muhammadilmi019@gmail.com>
2017-10-25 13:22:42 +0700

    masih ngebug

--- soal3.c
+++ soal3.c
@@ -8,72 +8,68 @@
 *compile dengan cara gcc -pthread -o [output] input.c *
 *******************************************************/
 
-pthread_t tid[2];//inisialisasi array untuk menampung thread dalam kasusu ini ada 2 thread
+pthread_t tid[3];//inisialisasi array untuk menampung thread dalam kasus ini ada 3 thread
 
 int st_lohan=100;
 int st_kepit=100;
-int input;
-void berimakan(){
-	printf("pilih :\n1. beri makan lohan\n2. beri makan kepiting");
-	scanf("%d,&input);
-	if(input==1){iter=iter+10;}
-	else if(input==2){iter1=iter1+10;}
-	else printf("input error");
-}
 void* nyawa(void *arg)
 {
     unsigned long i=0;
     pthread_t id=pthread_self();
-    int iter;
+    int a=1,b=1;
+    int input;
     if(pthread_equal(id,tid[0]))//thread untuk menjalankan counter
     {
-        system("clear");
-        for(iter=st_lohan;iter>0;)
+        while(st_lohan>0)
         {
-            printf("%i",iter);
-            fflush(stdout);
-            sleep(10);
-	    iter=iter-15;
-            system("clear");
-	if(iter>100){break;}
+            printf("lohan %i\n",st_lohan);
+            sleep(3);
+	    st_lohan-=15;
+	if(st_lohan>100){break;}
         }
 	printf("lohan udah mati");
+	a=0;
     }
     else if(pthread_equal(id,tid[1]))
     {
-        system("clear");
-        for(iter1=st_kepit;iter1>0;)
+        while(st_kepit>0)
         {
-            printf("%i",iter);
-            fflush(stdout);
-            sleep(20);
-	    iter1=iter1-10;
-            system("clear");
-	if(iter1>100){break;}
+            printf("kepiting %i\n",st_kepit);
+            sleep(5);
+	    st_kepit-=10;
+	if(st_kepit>100){break;}
         }
 	printf("kepiting udah mati");
+	b=0;
     }
-    }
+    if(pthread_equal(id,tid[2]))//thread untuk menjalankan counter
+    {
+	while(a==1&&b==1){
+        printf("pilih :\n1. beri makan lohan\n2. beri makan kepiting\n");
+	scanf("%d",&input);
+	if(input==1){st_lohan+=10;}
+	else if(input==2){st_kepit+=10;}
+	else printf("input error");}
+	if(st_lohan>100)st_lohan=0;
+	if(st_kepit>100)st_kepit=0;
+}
     return NULL;
 }
 int main(void)
 {
     int i=0;
     int err;
-    while(i<2)//looping membuat thread 2x
+    while(i<3)//looping membuat thread 2x
     {
         err=pthread_create(&(tid[i]),NULL,&nyawa,NULL);//membuat thread
         if(err!=0)//cek error
         {
             printf("\n can't create thread : [%s]",strerror(err));
         }
-        else
-        {
-            printf("\n create thread success");
-        }
         i++;
     }
     pthread_join(tid[0],NULL);
     pthread_join(tid[1],NULL);
+    pthread_join(tid[2],NULL);
     return 0;
 }
